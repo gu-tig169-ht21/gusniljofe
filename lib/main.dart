4cbdffa06a3a7056d21bfae4549f5ab01896e1c8
multihaxxor <samcrolol@gmail.com>
2021-11-26 11:34:58 +0100

    FilterWorking

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:my_first_app/widgets.dart';
 import './itemmanager.dart';
 import 'additem.dart';
- 
+
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -20,38 +22,41 @@ class MyApp extends StatelessWidget {
     );
   }
 }
- 
+
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key? key, required this.title}) : super(key: key);
- 
+
   final String title;
- 
+
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
- 
+
 class _MyHomePageState extends State<MyHomePage> {
-  ListManager myListManager = new ListManager();
- 
-  void _addItem(AddItem Item) {
+  ListManager myListManager = ListManager();
+
+  List<ListManager> list = <ListManager>[];
+  String activeFilter = 'All';
+
+  void _addItem(ShoppingItem Item) {
     setState(() {
       myListManager.addShoppingItem(Item);
     });
   }
- 
+
   void _toggleStatus(int index) {
     setState(() {
       myListManager.markStatus(index: index);
     });
   }
- 
+
   void _removeItem(int index) {
     setState(() {
       myListManager.removeShoppingItem(
           id: myListManager.ShoppingItems[index].id);
     });
   }
- 
+
   Icon _getCheckboxIcon(int index) {
     return myListManager.ShoppingItems[index].status
         ? Icon(
@@ -65,93 +70,88 @@ class _MyHomePageState extends State<MyHomePage> {
             color: Colors.lightBlue[400],
           );
   }
- 
+
   @override
   Widget build(BuildContext context) {
+    var filteredList = _filter(myListManager.ShoppingItems);
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
           actions: [
-            _dropDownMenu(),
+            PopupMenuButton(
+                itemBuilder: (context) => [
+                      const PopupMenuItem(child: Text('All'), value: 'All'),
+                      const PopupMenuItem(child: Text('Done'), value: 'Done'),
+                      const PopupMenuItem(
+                          child: Text('Undone'), value: 'Undone'),
+                    ],
+                onSelected: (String value) {
+                  setState(() {
+                    activeFilter = value;
+                  });
+                }),
           ],
         ),
-        body: ListView.builder(
-            itemCount: myListManager.ShoppingItems.length,
-            itemBuilder: (context, index) {
-              return Card(
-                key: UniqueKey(),
-                child: ListTile(
-                  title: Text(
-                    myListManager.ShoppingItems[index].title,
-                    style: myListManager.ShoppingItems[index].status
-                        ? TextStyle(decoration: TextDecoration.lineThrough)
-                        : TextStyle(decoration: TextDecoration.none),
-                  ),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      IconButton(
-                        icon: _getCheckboxIcon(index),
-                        onPressed: () {
-                          _toggleStatus(index);
-                        },
-                      ),
-                      IconButton(
-                        icon: Icon(
-                          Icons.delete_outline,
-                          size: 20.0,
-                          color: Colors.lightBlue[400],
-                        ),
-                        onPressed: () {
-                          _removeItem(index);
-                        },
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            }),
+        body: _buildbody(filteredList),
         floatingActionButton: PopupForm(callback: _addItem));
   }
-}
- 
-Widget _dropDownMenu() {
-  List<String> filterBy = ['all', 'done', 'undone'];
- 
-  return PopupMenuButton<String>(
-      icon: Icon(Icons.filter_list),
-      onSelected: altAction,
-      itemBuilder: (BuildContext context) {
-        return filterBy.map((String alt) {
-          return PopupMenuItem(
-            value: alt,
-            child: Text(alt),
+
+  Widget _buildbody(List<ShoppingItem> filteredList) {
+    return ListView.builder(
+        itemCount: filteredList.length,
+        itemBuilder: (context, index) {
+          return Card(
+            key: UniqueKey(),
+            child: ListTile(
+              title: Text(
+                filteredList[index].title,
+                style: filteredList[index].status
+                    ? TextStyle(decoration: TextDecoration.lineThrough)
+                    : TextStyle(decoration: TextDecoration.none),
+              ),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  IconButton(
+                    icon: _getCheckboxIcon(index),
+                    onPressed: () {
+                      _toggleStatus(index);
+                    },
+                  ),
+                  IconButton(
+                    icon: Icon(
+                      Icons.delete_outline,
+                      size: 20.0,
+                      color: Colors.lightBlue[400],
+                    ),
+                    onPressed: () {
+                      _removeItem(index);
+                    },
+                  ),
+                ],
+              ),
+            ),
           );
-        }).toList();
-      });
-}
- 
-void altAction(String alt) {
-  if (alt == 'all') {
-    print('all items');
-  } else if (alt == 'done') {
-    print('done items');
-  } else if (alt == 'undone') {
-    print('undone items');
+        });
+  }
+
+  List<ShoppingItem> _filter(List<ShoppingItem> listToFilter) {
+    if (activeFilter == 'Done') {
+      return listToFilter
+          .where((ShoppingItem item) => item.status == true)
+          .toList();
+    }
+    if (activeFilter == 'Undone') {
+      return listToFilter
+          .where((ShoppingItem item) => item.status == false)
+          .toList();
+    }
+    return listToFilter;
+  }
+
+  void setComplete(ShoppingItem check) {
+    setState(() {
+      check.status = !check.status;
+    });
   }
 }
- 
- 
-/*
-void altAction(String filterBy) {
-  if (filterBy == 'all') {
-    return ;
-  } else if (filterBy == 'done') {
-    var list;
-    return list.where((item) =>);
-  } else if (filterBy == 'undone') {
-    return;
-  }
-}
- 
-*/
